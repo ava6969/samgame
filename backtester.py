ee8f1449938d68ba1ddd976d7e931ec9eb4358f7
Jon_Bray <jon@augmentiqs.com>
2021-02-28 14:56:33 -0500

    Quantstats parameters added for D,W,M,A functions. Needs metrics and periods fixed, and should be all set.

--- backtester.py
+++ backtester.py
@@ -78,13 +78,13 @@ def daily_backtest(account_balances, daily_returns, metrics):
     d_sharpe_ratio = qs.stats.sharpe(daily_returns, periods=1, annualize=False)
     d_win_rate = qs.stats.win_rate(daily_returns)
     d_volatility = qs.stats.volatility(daily_returns, periods=1, annualize=False)
-    d_sortino_ratio = qs.stats.sortino()
-    d_calmar_ratio = qs.stats.calmar()
-    d_recovery_factor = qs.stats.recovery_factor()
-    d_risk_return_ratio = qs.stats.risk_return_ratio()
-    d_max_drawdown = qs.stats.max_drawdown()
-    d_drawdown_details = qs.stats.drawdown_details()
-    d_alpha, d_beta = qs.stats.greeks(daily_returns, SPY / BTC? / cryptoETF?, periods = 7)
+    d_sortino_ratio = qs.stats.sortino(daily_returns, periods=1, annualize=False)
+    d_calmar_ratio = qs.stats.calmar(daily_returns)
+    d_recovery_factor = qs.stats.recovery_factor(daily_returns)
+    d_risk_return_ratio = qs.stats.risk_return_ratio(daily_returns)
+    d_max_drawdown = qs.stats.max_drawdown(account_balances)
+    d_drawdown_details = qs.stats.drawdown_details(d_max_drawdown)
+    d_alpha, d_beta = qs.stats.greeks(daily_returns, SPY / BTC? / cryptoETF?, periods = 1)
 
     return d_sharpe_ratio,d_win_rate,d_volatility,d_sortino_ratio,d_calmar_ratio,d_recovery_factor, \
            d_risk_return_ratio,d_max_drawdown,d_drawdown_details,d_alpha,d_beta
@@ -105,12 +105,12 @@ def weekly_backtest(account_balances, weekly_returns,
     w_sharpe_ratio = qs.stats.sharpe(weekly_returns, periods=7, annualize=False)
     w_win_rate = qs.stats.win_rate(weekly_returns)
     w_volatility = qs.stats.volatility(weekly_returns, periods=7, annualize=False)
-    w_sortino_ratio = qs.stats.sortino()
-    w_calmar_ratio = qs.stats.calmar()
-    w_recovery_factor = qs.stats.recovery_factor()
-    w_risk_return_ratio = qs.stats.risk_return_ratio()
-    w_max_drawdown = qs.stats.max_drawdown()
-    w_drawdown_details = qs.stats.drawdown_details()
+    w_sortino_ratio = qs.stats.sortino(weekly_returns, periods=7, annualize=False)
+    w_calmar_ratio = qs.stats.calmar(weekly_returns)
+    w_recovery_factor = qs.stats.recovery_factor(weekly_returns)
+    w_risk_return_ratio = qs.stats.risk_return_ratio(weekly_returns)
+    w_max_drawdown = qs.stats.max_drawdown(account_balances)
+    w_drawdown_details = qs.stats.drawdown_details(w_max_drawdown)
     w_alpha, w_beta = qs.stats.greeks(weekly_returns, SPY / BTC? / cryptoETF?, periods=7)
 
     return w_sharpe_ratio,w_win_rate,w_volatility,w_sortino_ratio,w_calmar_ratio,w_recovery_factor, \
@@ -121,12 +121,12 @@ def monthly_backtest(account_balances, monthly_returns, metrics):
     m_sharpe_ratio = qs.stats.sharpe(monthly_returns, periods=30, annualize=False)
     m_win_rate = qs.stats.win_rate(monthly_returns)
     m_volatility = qs.stats.volatility(monthly_returns, periods=30, annualize=False)
-    m_sortino_ratio = qs.stats.sortino()
-    m_calmar_ratio = qs.stats.calmar()
-    m_recovery_factor = qs.stats.recovery_factor()
-    m_risk_return_ratio = qs.stats.risk_return_ratio()
-    m_max_drawdown = qs.stats.max_drawdown()
-    m_drawdown_details = qs.stats.drawdown_details()
+    m_sortino_ratio = qs.stats.sortino(monthly_returns)
+    m_calmar_ratio = qs.stats.calmar(monthly_returns)
+    m_recovery_factor = qs.stats.recovery_factor(monthly_returns)
+    m_risk_return_ratio = qs.stats.risk_return_ratio(monthly_returns)
+    m_max_drawdown = qs.stats.max_drawdown(account_balances)
+    m_drawdown_details = qs.stats.drawdown_details(m_max_drawdown)
     m_alpha, m_beta = qs.stats.greeks(monthly_returns, SPY / BTC? / cryptoETF?, periods = 30)
 
     return m_sharpe_ratio, m_win_rate, m_volatility, m_sortino_ratio, m_calmar_ratio, m_recovery_factor, \
@@ -137,16 +137,23 @@ def annual_backtest(account_balances, annual_returns, metrics):
     a_sharpe_ratio = qs.stats.sharpe(annual_returns, periods=365, annualize=True)
     a_win_rate = qs.stats.win_rate(annual_returns)
     a_volatility = qs.stats.volatility(annual_returns, periods=365, annualize=True)
-    a_sortino_ratio = qs.stats.sortino()
-    a_calmar_ratio = qs.stats.calmar()
-    a_recovery_factor = qs.stats.recovery_factor()
-    a_risk_return_ratio = qs.stats.risk_return_ratio()
-    a_max_drawdown = qs.stats.max_drawdown()
-    a_drawdown_details = qs.stats.drawdown_details()
+    a_sortino_ratio = qs.stats.sortino(annual_returns)
+    a_calmar_ratio = qs.stats.calmar(annual_returns)
+    a_recovery_factor = qs.stats.recovery_factor(annual_returns)
+    a_risk_return_ratio = qs.stats.risk_return_ratio(annual_returns)
+    a_max_drawdown = qs.stats.max_drawdown(account_balances)
+    a_drawdown_details = qs.stats.drawdown_details(a_max_drawdown)
     a_alpha, a_beta = qs.stats.greeks(annual_returns, SPY / BTC? / cryptoETF?, periods=365)
 
     return a_sharpe_ratio, a_win_rate, a_volatility, a_sortino_ratio, a_calmar_ratio, a_recovery_factor, \
            a_risk_return_ratio, a_max_drawdown, a_drawdown_details, a_alpha, a_beta
+
+
+
+
+
+
+
 
 #def weekly_backtest_all(weekly_returns):
 #    """
